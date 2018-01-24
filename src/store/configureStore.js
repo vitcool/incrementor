f33f6ae4fb54dbb9cf918170ff3d76059f08b667
Vitalii Kulyk <vitcool@ukr.net>
2018-01-24 22:14:45 +0200

    IC-1: primitive redux-saga is added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,22 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import tasks from "../modules/tasks";
+import createSagaMiddleware from "redux-saga";
+import helloSaga from "../sagas/saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const rootReducer = combineReducers({
+  tasks
+});
 
 const configureStore = initialState => {
-  const rootReducer = combineReducers({
-    tasks
-  });
-  return createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(sagaMiddleware)
+  );
+  sagaMiddleware.run(helloSaga);
+  return store;
 };
 
 export default configureStore;
